89a1ea6069a5ec975361adc97635ce53957827cd
foo2810 <ntshp.ak@gmail.com>
2021-03-09 17:41:27 +0900

    update move_survey_docs.py (prod)

--- move_survey_docs.py
+++ move_survey_docs.py
@@ -1,6 +1,7 @@
 import pprint
 import numpy as np
-from utils import get_page_list_by_page, create_page
+from utils import get_page_list_by_page, rename_page
+from utils import GrowiAPIError
 
 base_url = 'https://<domain>'
 api_token = 'api_token'
@@ -13,8 +14,17 @@ for user in users:
     for page in page_list:
         path = page['path']
         name = path.split('/')[-1]
+
+        if name == 'survey' or name == '_template' or name == '__template': continue
+
         new_path = f'/survey/{name}'
         if name == ['__template', '_template', 'survey'] : continue
         print(f'{path} -> {new_path}')
+
+        try:
+            res = rename_page(base_url, api_token, path, new_path, True)
+        except GrowiAPIError as e:
+            print(f' >>> Error:\n{e}')
+
         # pprint.pprint(page)
     print('='*100)
